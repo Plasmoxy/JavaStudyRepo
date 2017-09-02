c36066a6b8f65f660505ea583d00cf74e8ed42aa
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2017-09-03 00:02:10 +0200

    apparently running again

--- HFJava/beersong/BeerSong.java
+++ HFJava/beersong/BeerSong.java
@@ -4,7 +4,9 @@ public class BeerSong {
 	
 	public static void main(String[] args)
 	{
-		System.out.println("xd xdd"); 
+		System.out.println("xd xdd");
+		System.out.println("this sum shit");
+		System.out.println("amzign");
 	}
 
 }
